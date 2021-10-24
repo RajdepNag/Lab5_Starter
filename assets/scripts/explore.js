0e05d0c8285c796316e97157923cc659238b84f9
Rajdeep <rnag@ucsd.edu>
2021-10-24 16:42:49 -0700

    finished

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,35 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+//const voices = window.speechSynthesis.getVoices();
+
+setTimeout(() => {
+  var voices = window.speechSynthesis.getVoices();
+  populate_dropdown();
+}, 50);
+
+function populate_dropdown() {
+  const dropdown = document.getElementsByName("voice");
+
+   //console.log(voices.length);
+    
+    for(let i = 1; i <= voices.length; i++) {
+
+      var option = document.createElement('option');
+      
+      option.value = voices[i];
+      
+      dropdown.append(option);
+
+  
+  }
+  
+
+}
 
 function init() {
   // TODO
+
+
 }
+
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -7,18 +7,16 @@ const jsConfetti = new JSConfetti();
 
 
 function init() {
-  // TODO
+
   const horn = document.querySelector('#horn-select'); 
   const sound = document.getElementsByTagName("audio");
   const page_images = document.getElementsByTagName("img");
-  //console.log(page_images.length);
-  
-  
-    
+      
   horn.addEventListener('click', function(){
-   // console.log(horn.value);
+  
    if (horn.value != "select") {
      page_images[0].src = "assets/images/" + horn.value + ".svg"; 
+
      sound.src = "assets/audio/" + horn.value + ".mp3";
     
     }
@@ -26,22 +24,22 @@ function init() {
   });  
 
   const noise = document.getElementById('volume');
-  //console.log(noise.value);
+  
 
   noise.addEventListener('change', function(){
-    console.log(noise.value);
+    
     if (noise.value == 0) 
 
       page_images[1].src = "assets/icons/volume-level-0.svg";
-
-    else if (noise.value >= 1 && noise.value < 33) {
-
-      page_images[1].src = "assets/icons/volume-level-1.svg"; 
+      
+      else if (noise.value >= 1 && noise.value < 33) {
+        
+        page_images[1].src = "assets/icons/volume-level-1.svg"; 
 
     }
 
     else if (noise.value >= 33 && noise.value < 67) {
-
+      
       page_images[1].src = "assets/icons/volume-level-2.svg"; 
 
     }
@@ -54,13 +52,14 @@ function init() {
 
 const play_sound = document.querySelector('button');
 const audio_name = document.querySelector('audio');
-console.log(audio_name);
+
   
 play_sound.addEventListener('click', function(){
   
   if (horn.value != "select") {
     
     audio_name.src = "assets/audio/" + horn.value + ".mp3";
+    
     audio_name.volume = noise.value/parseFloat(100);
 
     if(horn.value == "party-horn" && noise.value != 0) {
